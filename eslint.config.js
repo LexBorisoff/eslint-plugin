e12610725fd2a83c833ede8153508235d49d3ccd
Lex Borisoff <lex@borisoff.dev>
2024-12-02 21:47:24 -0500

    chore: added eslint config

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+import tseslint from 'typescript-eslint';
+
+import plugin from './eslint/plugin/index.js';
+import { ignoreFile } from './eslint/utils/ignore-file.js';
+
+export default tseslint.config(
+  ignoreFile('.gitignore', import.meta),
+  { ignores: ['eslint'] },
+  plugin.configs.recommended,
+  plugin.configs.typescript,
+  {
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        projectService: {
+          allowDefaultProject: ['*.js'],
+        },
+      },
+    },
+  },
+);
