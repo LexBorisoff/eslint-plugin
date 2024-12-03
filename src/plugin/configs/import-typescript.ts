e02ff3690af92ee16cd7a6092a9d568ce99e9844
Lex Borisoff <lex@borisoff.dev>
2024-12-02 21:48:47 -0500

    feat: eslint plugin

---
+++ src/plugin/configs/import-typescript.ts
@@ -0,0 +1,12 @@
+import importPlugin from 'eslint-plugin-import';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(importPlugin.flatConfigs.typescript, {
+  settings: {
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+      },
+    },
+  },
+});
