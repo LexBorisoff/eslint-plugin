53c812bcccf69f9f88384c21fd10432e33c17cd6
Lex Borisoff <lex@borisoff.dev>
2024-12-02 22:49:00 -0500

    chore: renamed ignoreFile function to useIgnoreFile

--- eslint/utils/use-ignore-file.js
+++ eslint/utils/use-ignore-file.js
@@ -1,7 +1,7 @@
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { includeIgnoreFile } from '@eslint/compat';
-export function ignoreFile(file, meta) {
+export function useIgnoreFile(file, meta) {
     const __filename = fileURLToPath(meta.url);
     const __dirname = path.dirname(__filename);
     const gitignorePath = path.resolve(__dirname, file);
--- src/utils/use-ignore-file.ts
+++ src/utils/use-ignore-file.ts
@@ -3,7 +3,7 @@ import { fileURLToPath } from 'node:url';
 
 import { includeIgnoreFile, type FlatConfig } from '@eslint/compat';
 
-export function ignoreFile(file: string, meta: ImportMeta): FlatConfig {
+export function useIgnoreFile(file: string, meta: ImportMeta): FlatConfig {
   const __filename = fileURLToPath(meta.url);
   const __dirname = path.dirname(__filename);
   const gitignorePath = path.resolve(__dirname, file);
