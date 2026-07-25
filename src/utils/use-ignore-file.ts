0bc75e6b534f93567c066f237f97f402b308f40a
Lex Borisoff <lex@borisoff.dev>
2026-07-24 20:14:48 -0400

    fix: updated useIgnoreFile function

--- src/utils/use-ignore-file.ts
+++ src/utils/use-ignore-file.ts
@@ -1,11 +1,19 @@
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 
-import { includeIgnoreFile, type FlatConfig } from '@eslint/compat';
+import {
+  includeIgnoreFile,
+  type ConfigObject,
+  type IncludeIgnoreFileOptions,
+} from '@eslint/config-helpers';
 
-export function useIgnoreFile(file: string, meta: ImportMeta): FlatConfig {
+export function useIgnoreFile(
+  file: string,
+  meta: ImportMeta,
+  options?: IncludeIgnoreFileOptions,
+): ConfigObject {
   const __filename = fileURLToPath(meta.url);
   const __dirname = path.dirname(__filename);
   const gitignorePath = path.resolve(__dirname, file);
-  return includeIgnoreFile(gitignorePath);
+  return includeIgnoreFile(gitignorePath, options);
 }
