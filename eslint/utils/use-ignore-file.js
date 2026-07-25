df2d821866d7c51b3f28e57d2ab4f60f187a0d13
Lex Borisoff <lex@borisoff.dev>
2026-07-24 20:19:45 -0400

    chore: rebuilt use-ignore-file.js in eslint folder

--- eslint/utils/use-ignore-file.js
+++ eslint/utils/use-ignore-file.js
@@ -1,9 +1,9 @@
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
-import { includeIgnoreFile } from '@eslint/compat';
-export function useIgnoreFile(file, meta) {
+import { includeIgnoreFile, } from '@eslint/config-helpers';
+export function useIgnoreFile(file, meta, options) {
     const __filename = fileURLToPath(meta.url);
     const __dirname = path.dirname(__filename);
     const gitignorePath = path.resolve(__dirname, file);
-    return includeIgnoreFile(gitignorePath);
+    return includeIgnoreFile(gitignorePath, options);
 }
